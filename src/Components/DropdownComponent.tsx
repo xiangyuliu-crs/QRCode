491f51e26d30105979dc4c2151bbe88e1ffa0e1f
Xiangyu Liu <xiangyu.liu@crssoftware.co.nz>
2022-07-13 15:00:37 +1200

    add 5 options constraint

--- src/Components/DropdownComponent.tsx
+++ src/Components/DropdownComponent.tsx
@@ -12,8 +12,15 @@ interface DropdownListProps {
   onChange: (e) => void;
 }
 
+const OPTIONS_SIZE_LIMIT = 5;
+
 const DropdownList: React.FC<DropdownListProps> = (props) => {
   const { options, placeholder, selectedValue, onChange } = props;
+
+  const optionsForDropdown =
+    options.length > OPTIONS_SIZE_LIMIT
+      ? options.slice(0, OPTIONS_SIZE_LIMIT)
+      : options;
 
   return (
     <React.Fragment>
@@ -23,7 +30,7 @@ const DropdownList: React.FC<DropdownListProps> = (props) => {
             {placeholder.label}
           </option>
         )}
-        {options.map((opt) => (
+        {optionsForDropdown.map((opt) => (
           <option value={opt.value}>{opt.label}</option>
         ))}
       </select>
